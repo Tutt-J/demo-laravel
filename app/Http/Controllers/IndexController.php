a106dc208a28ef8d8c72b556b88441a648d329d3
Wy <wy@mail.com>
2023-02-07 10:57:54 +0100

    create domain model and put it dynamically on homepage

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Domain;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $domains = Domain::all();
+        return view('home',  ['domains' => $domains]);
     }
 }
