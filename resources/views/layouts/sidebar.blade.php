2defd4101f72d5f2f5cbae78927634d9646b4e9a
Wy <wy@mail.com>
2023-02-07 16:51:09 +0100

    Do logout

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,39 +1,47 @@
 <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-      <span class="fs-4">Sidebar</span>
+        <svg class="bi me-2" width="40" height="32">
+            <use xlink:href="#bootstrap" />
+        </svg>
+        <span class="fs-4">Sidebar</span>
     </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item">
-        <a href="#" class="nav-link active" aria-current="page">
-          Tableau de bord
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          Domaines d'expertise
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          Services
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          Portfolio
-        </a>
-      </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link active" aria-current="page">
+                Tableau de bord
+            </a>
+        </li>
+        <li>
+            <a href="#" class="nav-link text-white">
+                Domaines d'expertise
+            </a>
+        </li>
+        <li>
+            <a href="#" class="nav-link text-white">
+                Services
+            </a>
+        </li>
+        <li>
+            <a href="#" class="nav-link text-white">
+                Portfolio
+            </a>
+        </li>
     </ul>
     <hr>
     <div class="dropdown">
-      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-        <strong>mdo</strong>
-      </a>
-      <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-        <li><a class="dropdown-item" href="#">Sign out</a></li>
-      </ul>
+        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+            id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://github.com/mdo.png" alt="" width="32" height="32"
+                class="rounded-circle me-2">
+            <strong>mdo</strong>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+            <li><a href="{{ route('logout') }}" class="dropdown-item"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </ul>
     </div>
-  </div>
+</div>
